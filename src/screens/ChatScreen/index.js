dd3758baaac292b0e650a92a51424dc6634ebefb
bshawn <bret.shawn@gmail.com>
2017-11-02 00:48:29 -0400

    Copy example into chat screen

--- src/screens/ChatScreen/index.js
+++ src/screens/ChatScreen/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+
 import styles from '../styles';
 
 export default class ChatScreen extends React.Component {
@@ -7,11 +8,45 @@ export default class ChatScreen extends React.Component {
     title: 'Chat'
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://facebook.github.io/react/img/logo_og.png',
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This is a chat screen</Text>
-      </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     );
   }
 }
