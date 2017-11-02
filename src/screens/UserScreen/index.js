aaab1259026c6006b8a6a272d23f182cf24de960
bshawn <bret.shawn@gmail.com>
2017-11-01 20:59:07 -0400

    Create form and update state

--- src/screens/UserScreen/index.js
+++ src/screens/UserScreen/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
 import styles from '../styles';
 
 export default class UserScreen extends React.Component {
@@ -7,10 +8,50 @@ export default class UserScreen extends React.Component {
     title: 'User'
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: '',
+      userName: '',
+      fullName: ''
+    };
+
+    this.onUserNameChanged = this.onUserNameChanged.bind(this);
+    this.onFullNameChanged = this.onFullNameChanged.bind(this);
+  }
+
+  onUserNameChanged(value) {
+    this.setState({
+      userName: value
+    });
+  }
+
+  onFullNameChanged(value) {
+    this.setState({
+      fullName: value
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This is a user screen</Text>
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <FormLabel>User Name</FormLabel>
+        <FormInput
+          placeholder="Enter a user name"
+          onChangeText={this.onUserNameChanged}
+        />
+
+        <FormLabel>Full Name</FormLabel>
+        <FormInput
+          placeholder="Enter first and last name (optional)"
+          onChangeText={this.onFullNameChanged}
+        />
+        <Button
+          title="Save"
+        />
+        <FormValidationMessage>
+        </FormValidationMessage>
       </View>
     );
   }
